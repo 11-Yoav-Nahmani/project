44a9cb4ae383621b18e6e661b03e6823440cc91e
rr <nadavv005@gmail.com>
2022-06-23 11:50:09 +0300

    upload

---
+++ bridges/src/Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+
+    private int wheels;
+    private int doors;
+    private String engine;
+
+    public Vehicle(int wheels,int doors, String engine){
+        this.wheels = wheels;
+        this.doors = doors;
+        this.engine = engine;
+    }
+
+}
